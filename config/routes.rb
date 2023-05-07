d733647a2bceb68587fc076df6878579cc826951
yada-kent <42689607+yada-kent@users.noreply.github.com>
2023-05-07 12:39:59 +0000

    編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get '/books' => 'books#new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  # get '/books' => 'books#index'
+  # get '/show' => 'books#show'
+  # get '/edit' => 'books#edit'
+  post 'books' => 'books#create'
   get '/top' => 'homes#top'
+  get 'books/:id' => 'books#show' , as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book' 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
